e08e7c001ead9680ed643dc2a5cc26429a55feee
mkarpacka <209348@edu.p.lodz.pl>
2019-12-15 18:32:04 +0100

    usunięcie nieużywanej listy, poprawa strony domowej

--- shoppingApp/src/app/app-routing.module.ts
+++ shoppingApp/src/app/app-routing.module.ts
@@ -12,10 +12,6 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
-    path: 'list',
-    loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-  },
-  {
     path: 'shopping-lists',
     loadChildren: () => import('./shopping-lists/shopping-lists.module').then(m=>m.ShoppingListsModule)
   },
